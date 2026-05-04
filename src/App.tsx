cd4f23c6a1e36b15d1690d2b121e2c5de14b7e17
oussama-eljarrari <oussamaeljarrari631@gmail.com>
2026-05-04 21:13:47 +0100

    Homepage v1.0

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ export function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/home" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/gig/:id" element={<GigDetailPage />} />
+      
     </Routes>
 
   )
